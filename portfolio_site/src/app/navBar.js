a5167c3041472449659ad091f851ea88d1e1c014
JuanTime2Late <juan.vera05@utrgv.edu>
2024-07-26 05:30:12 -0500

    Home page updated

--- portfolio_site/src/app/navBar.js
+++ portfolio_site/src/app/navBar.js
@@ -12,13 +12,11 @@ const NavBar = () => {
     return (
         <>
         <nav className="bg-black">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
                         <div className="flex-shrink-0">
-                            <p className="text-white">
-                                Juan Vera
-                            </p>
+                            <img className="text-white" src="JuanVera3d.png" alt="Juan Vera" ></img>
                         </div>
                     </div>
                     <div className="hidden md:block">
--- portfolio_site/src/app/page.js
+++ portfolio_site/src/app/page.js
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <>
       <NavBar />
+      <div className="flex h-screen items-center bg-zinc-900 shadow-white 
+      shadow-[inset_0_4px_6px_-3px_rgba(0,0,0,0.9)]"> 
+        <div className="flex justify-evenly items-center h-5/6 w-screen bg-black shadow-zinc-950 text-white 
+        shadow-[0_-7px_10px_-1px_rgba(0,0,0,0.1),_0_7px_10px_-1px_rgba(0,0,0,0.1)]">
+          <p>Recent Computer Science Bachelor graduate with great enthusiasm for learning and programming.</p>
+          <img className=" h-12 w-32" src="JuanVera3d.png" alt="Juan Vera" ></img>
+        </div>
+      </div>
     </>
   );
 }
