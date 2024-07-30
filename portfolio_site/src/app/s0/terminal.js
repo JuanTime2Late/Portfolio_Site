b293c6498431550dd05800ef59b6b56346316644
JuanTime2Late <juan.vera05@utrgv.edu>
2024-07-30 15:57:37 -0500

    Update Terminal Page

--- portfolio_site/src/app/s0/terminal.js
+++ portfolio_site/src/app/s0/terminal.js
@@ -1,7 +1,15 @@
 "use client";
-import React, { useState }from "react";
+import React, { useEffect, useRef, useState }from "react";
 
 const Terminal = () => {
+    const iRef = useRef(null);
+
+    useEffect(() => { 
+        if (iRef.current) {
+            iRef.current.focus();
+        }
+    }, []);
+
     const [input, setInput] = useState('');
     const [output, setOutput] = useState([
         { type: 'output', text: 'Instructions: Type `contact` to get my info or `clear` to clear the screen.' }
@@ -22,32 +30,35 @@ const Terminal = () => {
         } else {
             setOutput([...output, 
                 { type: 'input', text: `user@letsimagine-desktop:~$ ${input}` }, 
-                { type: 'output', text: `You typed: ${input}` }]);
+                { type: 'error', text: `"${input}" does not exist as a command.` }]);
         }
         setInput('');
     };
 
     return(
         <div className="bg-black text-white h-screen p-4 font-mono animate-fadeIn">
-                <form onSubmit={handleSubmit} className="mt-2">
-                    <div className="flex items-center">
-                        <span className="text-green-500 mr-2">user@letsimagine-desktop:~$</span>
-                            <input
-                                id="terminal-input"
-                                type="text"
-                                className="bg-black text-white outline-none flex-grow"
-                                value={input}
-                                onChange={(a) => setInput(a.target.value)}
-                            />
-                    </div>
-                    <div className="h-64 overflow-y-auto">
-                        {output.map((line, index) => (
+            <form onSubmit={handleSubmit} className="mt-2">
+                <div className="flex items-center">
+                    <span className="text-green-500 mr-2">user@letsimagine-desktop:~$</span>
+                        <input id="terminal-input" ref={iRef} type="text" className="bg-transparent text-white outline-none flex-grow"
+                            value={input}
+                            onChange={(a) => setInput(a.target.value)}
+                        />
+                </div>
+                <div className="h-64 overflow-y-auto">
+                    {output.map((line, index) => (
+                        line.type === 'error' ? (
+                            <div key={index} className="text-red-600">
+                                &nbsp;&nbsp;{line.text}
+                            </div>
+                        ) : (
                             <div key={index} className={line.type === 'input' ? 'text-green-500' : ''}>
                                 {line.text}
                             </div>
-                        ))}
-                    </div>
-                </form>
+                        )
+                    ))}
+                </div>
+            </form>
         </div>
     );
 
