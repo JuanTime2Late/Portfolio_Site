721d8e6510c0fac6f9b26d7a11a5ce175fe36bc6
JuanTime2Late <juan.vera05@utrgv.edu>
2024-08-14 01:59:15 -0500

    Update Terminal

--- portfolio_site/src/app/s0/terminal.js
+++ portfolio_site/src/app/s0/terminal.js
@@ -12,14 +12,14 @@ const Terminal = () => {
 
   const [input, setInput] = useState('');
   const [output, setOutput] = useState([
-    { type: 'output', text: 'Instructions: Type `contact` to get my info or `clear` to clear the screen.' }
+    { type: 'output', text: '' }
   ]);
 
   const handleSubmit = (entry) => {
     entry.preventDefault();
 
     if (input.trim().toLowerCase() === 'clear' || input.trim().toLowerCase() === 'cls') {
-      setOutput([{ type: 'output', text: 'Instructions: Type `contact` to get my info or `clear` to clear the screen.' }]);
+      setOutput([{ type: 'output', text: '' }]);
     } 
     else if(input.trim().toLowerCase() === 'contact') {
       setOutput([...output, 
@@ -37,11 +37,15 @@ const Terminal = () => {
   };
 
   return(
-    <div className=" text-white h-screen p-4 font-mono">
+    <div className=" text-white text-xs sm:text-base h-screen p-4 font-mono">
       <form onSubmit={handleSubmit} className="bg-black rounded-md mt-2">
         <div className="flex items-center">
           <span className="text-green-500 mr-2">user@letsimagine-desktop:~$</span>
-            <input id="terminal-input" ref={iRef} type="text" className="bg-transparent text-white outline-none flex-grow"
+            <input id="terminal-input" 
+              ref={iRef} 
+              type="text" 
+              className="bg-transparent text-white outline-none flex-grow"
+              placeholder="<-- Type `contact` for info."
               value={input}
               onChange={(a) => setInput(a.target.value)}
             />
